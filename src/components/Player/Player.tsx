7381857f305819fdd8e79e989fc350f45208669a
Dominik Szalai <emptulik@gmail.com>
2024-12-15 20:00:24 +0100

    p79

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,22 +1,28 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 
 export interface PlayerProps {
-  name: string
+  initialName: string
   symbol: string
 }
 
-const Player = ({ name, symbol }: PlayerProps) => {
+const Player = ({ initialName, symbol }: PlayerProps) => {
+  const [name, setName] = useState<string>(initialName)
+
   const [isEditing, setIsEditing] = useState<boolean>(false)
 
   const onClickHandler = () => {
     setIsEditing(previous => !previous)
   }
 
+  const onNameChangeHandler = (x: FormEvent<HTMLInputElement>) => {
+    setName(x.currentTarget.value)
+  }
+
   return (
     <li>
       <span className="player">
         {isEditing
-          ? <input type="text" required value={name} />
+          ? <input type="text" required value={name} onChange={onNameChangeHandler} />
           : <span className="player-name">{name}</span>}
         <span className="player-symbol">{symbol}</span>
       </span>
