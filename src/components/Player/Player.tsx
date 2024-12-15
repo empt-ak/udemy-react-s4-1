98ed4a9cb1b687967aba10af234352bf092ed163
Dominik Szalai <emptulik@gmail.com>
2024-12-15 19:34:01 +0100

    p75

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,16 +1,24 @@
+import { useState } from 'react'
+
 export interface PlayerProps {
   name: string
   symbol: string
 }
 
 const Player = ({name, symbol}: PlayerProps) => {
+  const [isEditing, setIsEditing] = useState<boolean>(false)
+
+  const onClickHandler = () => {
+    setIsEditing(true)
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {isEditing ? <input  type="text" required /> : <span className="player-name">{name}</span>}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={onClickHandler}>Edit</button>
     </li>
   )
 }
